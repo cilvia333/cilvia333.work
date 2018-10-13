74ab43ff105946cacf2eafcd09f61ba1a7ef697c
cilvia <tech0970@gmail.com>
2018-10-14 00:10:28 +0900

    rename webname

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,7 +3,7 @@ module.exports = {
   ** Headers of the page
   */
   head: {
-    title: 'cilvia333-portfolio',
+    title: "Cilvia333's Official Website",
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
