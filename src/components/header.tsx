892b6489876aa48f8e059466c72c3f31e806e656
Cilvia333 <cilvia333x@gmail.com>
2020-08-09 05:11:46 +0900

    feat: Update index

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -11,7 +11,7 @@ const Header: React.FC<Props> = ({ siteTitle = `` }: Props) => {
     <CustomHeader>
       <Wrapper>
         <Title>
-          <TitleLink to="/">{siteTitle}</TitleLink>
+          <TitleLink to="/">cilvia333.work | {siteTitle}</TitleLink>
         </Title>
         <Menu>
           <li>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,38 @@
 import React from 'react';
 import { Link } from 'gatsby';
 import styled from 'styled-components';
+import tw from 'twin.macro';
 
 import Layout from '../components/layout';
 import Image from '../components/image';
 import SEO from '../components/seo';
 
+import IconImg from '../images/icon.jpg';
+
 const IndexPage: React.FC = () => {
   return (
     <Layout>
       <SEO title="" />
-      <h1>cilvia333.work</h1>
-      <Image filename="icon.jpg" alt="" />
+      <TopWrapper>
+        <Icon src={IconImg} alt="icon" />
+        <Title>cilvia333.work</Title>
+      </TopWrapper>
     </Layout>
   );
 };
+
+const TopWrapper = styled.div`
+  ${tw`flex justify-center items-center flex-col bg-gray-200`}
+`;
+
+const Icon = styled.img`
+  width: 210px;
+  height: 210px;
+  border-radius: 50%;
+`;
+
+const Title = styled.h1`
+  ${tw`font-header font-bold text-6xl`}
+`;
 
 export default IndexPage;
