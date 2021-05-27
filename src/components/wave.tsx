cf8aa0a05f342f12c0fdac6ec59800f0b7b5de36
cilvia333 <cilvia333x@gmail.com>
2021-05-27 17:21:12 +0900

    fix: 波のアニメーションを修正

--- src/components/wave.tsx
+++ src/components/wave.tsx
@@ -21,82 +21,147 @@ const Wave: React.FC<Props> = ({ className, color }: Props) => {
       <WaveImg color={color} />
       <WaveImg color={color} />
       <WaveImg color={color} />
+      <Bg color={color} />
     </WaveWrapper>
   );
 };
 
-const waveKeyframe = keyframes`
+const waveKeyframe1 = keyframes`
   0% {
     transform: translateX(0) translateY(0);
   }
   25% {
-    transform: translateX(-480px) translateY(30px);
+    transform: translateX(-480px) translateY(20px);
   }
   50% {
     transform: translateX(-960px) translateY(0);
   }
   75% {
-    transform: translateX(-1440px) translateY(-30px);
+    transform: translateX(-1440px) translateY(-20px);
   }
   100% {
     transform: translateX(-1920px) translateY(0px);
   }
 `;
 
+const waveKeyframe2 = keyframes`
+  0% {
+    transform: translateX(-480px) translateY(0);
+  }
+  25% {
+    transform: translateX(-960px) translateY(-10px);
+  }
+  50% {
+    transform: translateX(-1440px) translateY(0);
+  }
+  75% {
+    transform: translateX(-1920px) translateY(30px);
+  }
+  100% {
+    transform: translateX(-2400px) translateY(0);
+  }
+`;
+
+const waveKeyframe3 = keyframes`
+  0% {
+    transform: translateX(-760px) translateY(0);
+  }
+  25% {
+    transform: translateX(-1240px) translateY(40px);
+  }
+  50% {
+    transform: translateX(-1720px) translateY(0);
+  }
+  75% {
+    transform: translateX(-2200px) translateY(40px);
+  }
+  100% {
+    transform: translateX(-2680px) translateY(0px);
+  }
+`;
+
 const WaveWrapper = styled.div`
-  ${tw`relative w-full overflow-hidden`}
+  ${tw`relative w-full`}
   height: 20vh;
 `;
 
 const WaveImg = styled.div<{ color: 'yellow' | 'white' }>`
-  ${tw`absolute h-full top-0`}
-  animation: ${waveKeyframe} 12s linear infinite 0s;
-  background: top left/1920px repeat-x ;
-  min-width: 3840px;
-  width: 200%;
+  ${tw`absolute h-full top-0 z-10`}
+  animation: linear infinite;
+  background: top left/1920px repeat-x;
+  min-width: 5760px;
+  width: 300%;
   opacity: 80%;
 
-  &:nth-child(1) {
+  &:nth-of-type(1) {
     left: -240px;
-    animation-delay: 0s;
-    animation-duration: 20.2s;
+    animation-name: ${waveKeyframe1};
+    animation-duration: 32.2s;
   }
 
-  &:nth-child(2) {
+  &:nth-of-type(2) {
     left: -120px;
-    animation-delay: 2s;
-    animation-duration: 16.5s;
+    animation-name: ${waveKeyframe2};
+    animation-duration: 24.5s;
   }
-  &:nth-child(3) {
+  &:nth-of-type(3) {
     left: 0;
-    animation-delay: 4s;
+    animation-name: ${waveKeyframe3};
     animation-duration: 14s;
   }
 
   ${({ color }) =>
     color === 'yellow'
       ? css`
-          &:nth-child(1) {
-            background-image: url(${yellowWave03});
+          &:nth-of-type(1) {
+            opacity: 40%;
+            background-image: url(${yellowWave});
           }
-          &:nth-child(2) {
-            background-image: url(${yellowWave02});
+          &:nth-of-type(2) {
+            opacity: 55%;
+            background-image: url(${yellowWave});
           }
-          &:nth-child(3) {
-            background-image: url(${yellowWave01});
+          &:nth-of-type(3) {
+            opacity: 80%;
+            background-image: url(${yellowWave});
           }
         `
       : css`
-          &:nth-child(1) {
+          &:nth-of-type(1) {
             background-image: url(${whiteWave03});
           }
 
-          &:nth-child(2) {
+          &:nth-of-type(2) {
             background-image: url(${whiteWave02});
           }
-          &:nth-child(3) {
+          &:nth-of-type(3) {
             background-image: url(${whiteWave01});
           }
+        `}
+`;
+
+const Bg = styled.div<{ color: 'yellow' | 'white' }>`
+  ${tw`absolute bottom-0 w-full `}
+
+  height: 50%;
+
+  ${({ color }) =>
+    color === 'yellow'
+      ? css`
+          background-image: linear-gradient(
+            to bottom,
+            rgba(252, 191, 59, 0),
+            rgba(252, 191, 59, 1),
+            rgba(252, 191, 59, 1)
+          );
+        `
+      : css`
+          background-image: linear-gradient(
+            to bottom,
+            rgba(255, 255, 255, 0),
+            rgba(255, 255, 255, 1),
+            rgba(255, 255, 255, 1)
+          );
         `}
 `;
 
