9f4033a8620a8d141c49da20a2f74abf153c81aa
cilvia333 <cilvia333x@gmail.com>
2021-09-07 08:09:53 +0900

    chore: clean up dir for v4

--- gatsby-browser.js
+++
@@ -1,8 +0,0 @@
-import 'tailwindcss/dist/base.min.css';
-
-export const onClientEntry = () => {
-  // IntersectionObserver polyfill for gatsby-background-image (Safari, IE)
-  if (!(`IntersectionObserver` in window)) {
-    import(`intersection-observer`);
-  }
-};
--- src/hooks/index.ts
+++
@@ -1,7 +0,0 @@
-import assetUrl from './useContentfulImage';
-
-export const useContentfulImage = (url: string) => assetUrl(url);
-
-export { layoutContext, useLayoutContext } from './useLayoutContext';
-
-export { useIntersectionObserver } from './useIntersectionObserver';
